8d3f4137dd1adac22b934a55177c08c75af22285
AdjieDjaeger <spiritjaeger@gmail.com>
2021-03-01 23:43:27 +0700

    feat(route): create auth routes

--- routes/auth/auth.routes.php
+++ routes/auth/auth.routes.php
@@ -3,3 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', 'AuthController@index')->name('login');
+Route::prefix('auth')->group(function () {
+    Route::post('login', 'AuthController@login')->name('auth.login');
+    Route::post('logout', 'AuthController@logout')->middleware('auth')->name('auth.logout');
+});
