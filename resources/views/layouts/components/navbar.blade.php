ee3e73932b03a72314acccfe9402b232f7fec910
AdjieDjaeger <spiritjaeger@gmail.com>
2021-03-02 22:30:46 +0700

    feat(): created navbar

---
+++ resources/views/layouts/components/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="flex justify-end w-full p-4">
+    <div class="flex items-center gap-3">
+        <a href="" class="flex items-center gap-3 px-4 py-2 transition duration-200 ease-in-out rounded-lg hover:text-blue-600">
+            <i class="fas fa-user"></i>
+            <span class="font-semibold">Profile</span>
+        </a>
+        <span class="text-2xl"> | </span>
+        <a href="" class="flex items-center gap-3 px-4 py-2 transition duration-200 ease-in-out rounded-lg hover:text-red-600">
+            <i class="fas fa-power-off"></i>
+            <span class="font-semibold">Logout</span>
+        </a>
+    </div>
+</nav>
