efa89cc082f2a53ac0be99e69d8ee440b517c8df
AdjieDjaeger <spiritjaeger@gmail.com>
2021-03-02 22:30:32 +0700

    feat(): included navbar and sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
     <title>
         CS Manager | @yield('title')
     </title>
 </head>
 <body>
-    <main class="w-full h-screen">
-        @yield('content')
+    <main class="w-full h-screen @auth flex @endauth">
+        @guest
+            @yield('guestContent')
+        @endguest
+        @auth
+            @include('layouts.components.sidebar')
+            <div class="w-5/6 ">
+                @include('layouts.components.sidebar')                
+                <div>
+                    @yield('authContent')
+                </div>
+            </div>
+        @endauth
     </main>
 
     @stack('scripts')    
     @push('scripts')
         <script src="{{ asset('js/app.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js" integrity="sha512-UwcC/iaz5ziHX7V6LjSKaXgCuRRqbTp1QHpbOJ4l1nw2/boCfZ2KlFIqBUA/uRVF0onbREnY9do8rM/uT/ilqw==" crossorigin="anonymous"></script>
     @endpush
 </body>
 </html>
