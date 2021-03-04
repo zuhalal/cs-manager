41dab2af654b9d123b570ce8b3d5d2def74601f5
AdjieDjaeger <spiritjaeger@gmail.com>
2021-03-04 23:57:42 +0700

    feat(component): created info box

---
+++ resources/views/layouts/components/infoBox.blade.php
@@ -0,0 +1,7 @@
+<a href="" class="flex items-center gap-2 px-8 py-6 text-yellow-600 transition duration-200 ease-in-out bg-white rounded-lg group hover:text-white hover:bg-yellow-600">
+    <i class=" fas fa-{{ $icon }} fa-5x"></i>
+    <div class="flex flex-col items-center w-full gap-2">
+        <span class="font-semibold text-black group-hover:text-white">{{ $infoTitle }}</span>
+        <span class="font-bold">0</span>
+    </div>
+</a>
