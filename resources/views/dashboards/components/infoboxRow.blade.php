ae2541ec5c5acf92ad8606908ed31f5b4f5f91a8
AdjieDjaeger <spiritjaeger@gmail.com>
2021-03-04 23:48:21 +0700

    feat(component): created info box row

---
+++ resources/views/dashboards/components/infoboxRow.blade.php
@@ -0,0 +1,24 @@
+<div class="w-1/4">
+    @include('layouts.components.infoBox', [
+        'icon' => 'ad',
+        'infoTitle' => 'Campaigns'
+    ])
+</div>
+<div class="w-1/4">
+    @include('layouts.components.infoBox', [
+        'icon' => 'user-circle',
+        'infoTitle' => 'Operators'
+    ])
+</div>
+<div class="w-1/4">
+    @include('layouts.components.infoBox', [
+        'icon' => 'eye',
+        'infoTitle' => 'Visitors'
+    ])
+</div>
+<div class="w-1/4">
+    @include('layouts.components.infoBox', [
+        'icon' => 'shopping-cart',
+        'infoTitle' => 'Orders'
+    ])
+</div>
