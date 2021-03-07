aa6e4b2b0a28d045e45d9b769099639544aa868d
AdjieDjaeger <spiritjaeger@gmail.com>
2021-03-07 20:05:22 +0700

    fix(component): refactored sideebar

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -2,13 +2,5 @@
     <div class="py-6 text-center">
         <a href="{{ route('dashboard') }}" class="text-3xl font-extrabold text-white">CS Manager</a>
     </div>
-    <nav class="px-4 py-2 text-white">
-        <a href="{{ route('dashboard') }}" class="flex items-center gap-3 py-2 pl-4 transition duration-200 ease-in-out rounded-lg hover:bg-yellow-600        
-        @if(request()->segment(1) == 'dashboard')
-            bg-yellow-600
-        @endif">
-            <i class="fas fa-tachometer-alt fa-lg"></i>
-            <span class="text-lg font-semibold">Dashboard</span>
-        </a>
-    </nav>
+    @include('layouts.components.sidenav')
 </aside>
